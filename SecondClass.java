9793713c7aafc06f472ac34a419a3e98acc69a47
giobtu <55483070+giobtu@users.noreply.github.com>
2019-10-23 10:46:14 +0400

    btu
    initial

---
+++ SecondClass.java
@@ -0,0 +1,9 @@
+package HomeWork2;
+
+
+public interface SecondMainClass {
+	public void betweenNumbers();
+	public void separators();
+	public void primeSeparators();
+	public char commonNumber();
+}
